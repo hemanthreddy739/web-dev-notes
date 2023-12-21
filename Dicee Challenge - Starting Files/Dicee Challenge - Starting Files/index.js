321c0d55d529748c9cc13ef780b791cc443897a0
P HEMANTH KUMAR REDDY <93938266+hemanthreddy739@users.noreply.github.com>
2023-12-21 22:02:34 +0530

    Add files via upload

---
+++ Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,39 @@
+
+
+
+// one approach of random image selection
+
+var randomNumber1 = Math.floor((Math.random())*6)+1;
+var randomImagePath1 = "images/"+ "dice"+ randomNumber1+ ".png";
+document.querySelectorAll('img')[0].setAttribute("src", randomImagePath1);
+
+
+
+var randomNumber2 = Math.floor((Math.random())*6)+1;
+var randomImagePath2 = "images/"+ "dice"+ randomNumber2+ ".png";
+document.querySelector(".img2").setAttribute('src',randomImagePath2 );
+
+
+// declaring the winner
+
+
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector('h1').textContent = "🚩 Player1 wins!";
+}
+
+else if (randomNumber2 > randomNumber1){
+
+    document.querySelector('h1').textContent = 'Player 2 wins! 🚩' ;
+}
+
+else{
+
+    document.querySelector('h1').textContent = 'Draw';
+
+}
+
+
+
+
+
