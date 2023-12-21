9ff2a52946270a4de44e0043af230728a1addf63
P HEMANTH KUMAR REDDY <93938266+hemanthreddy739@users.noreply.github.com>
2023-12-21 22:05:15 +0530

    Add files via upload

---
+++ Jquery-Demo/index.js
@@ -0,0 +1,25 @@
+
+// $('button').eq(4).click(function (){
+//     $('h1').css('color', 'purple');
+// });
+
+
+// $('input').keydown(
+
+//     function(e) {
+        
+//             $('h1').text(e.key);
+
+            
+//     });
+
+
+// $('h1').css('color', 'orange');
+
+
+
+
+$('button').eq(0).on('click', function() {
+
+    $('h1').fadeIn();
+});
